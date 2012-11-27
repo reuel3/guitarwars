4e192612d5765b525df78ee8fbd5a309f98a3c7f
Skip Kleckner <reuel3@gmail.com>
2012-11-27 13:48:26 -0500

    changed db connect settings

--- connectvars.php
+++ connectvars.php
@@ -5,7 +5,7 @@
 	define('DB_HOST', 'int.instance32897.db.xeround.com');
 	define('DB_USER', 'app9522621');
 	define('DB_PASSWORD', 'VaCqEQGm');
-	define('DB_NAME', 'app9522621');
+	define('DB_NAME', 'gwdb');
 	/*define('DB_HOST', 'localhost');
 	define('DB_USER', 'root');
 	define('DB_PASSWORD', 'skip1234');
